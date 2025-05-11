07a4017b78eb9cc23fb0f3c1afa38464398afce5
Carlos <carloscuba014@gmail.com>
2025-05-10 20:24:08 -0400

    add support for dotenv

--- src/main/java/com/gfcf14/webdevtoons/WebdevtoonsApplication.java
+++ src/main/java/com/gfcf14/webdevtoons/WebdevtoonsApplication.java
@@ -3,10 +3,16 @@ package com.gfcf14.webdevtoons;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class WebdevtoonsApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(WebdevtoonsApplication.class, args);
 	}
 
