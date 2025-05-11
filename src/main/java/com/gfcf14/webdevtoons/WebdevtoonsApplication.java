cb342bf4e9812c2f14ae677f6c0ae63ee06b6d52
Carlos <carloscuba014@gmail.com>
2025-05-10 20:13:50 -0400

    initial commit

---
+++ src/main/java/com/gfcf14/webdevtoons/WebdevtoonsApplication.java
@@ -0,0 +1,13 @@
+package com.gfcf14.webdevtoons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdevtoonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebdevtoonsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gfcf14/webdevtoons/WebdevtoonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gfcf14.webdevtoons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebdevtoonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
