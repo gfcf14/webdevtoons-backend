8964ff32a00636fb86e2df3d12baa77b950492df
Carlos <carloscuba014@gmail.com>
2025-06-20 20:54:19 -0400

    refactor usage of frontend origin for cors

--- src/main/java/com/gfcf14/webdevtoons/config/WebConfig.java
+++ src/main/java/com/gfcf14/webdevtoons/config/WebConfig.java
@@ -1,12 +1,14 @@
 package com.gfcf14.webdevtoons.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-  private final String frontendOrigin = System.getProperty("FRONT_END_ORIGIN");
+  @Value("${frontend.origin}")
+  private String frontendOrigin;
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
